6ed08e2994d985abf2b98694ac432343be54de5f
akpathak1007 <anujkumarpathak1007@gmail.com>
2022-02-14 13:44:24 +0530

    stream app is completed

--- src/component/streams/StreamShow.js
+++ src/component/streams/StreamShow.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   id = this.props.match.params.id;
   componentDidMount() {
     this.props.fetchStream(this.id);
+    this.buildPlayer();
+  }
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
   render() {
     if (!this.props.stream) {
@@ -14,6 +37,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} controls style={{ width: "100%" }} />
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
